430ff6ce0a45726998abf4b71fecdd3e3bc786c9
Michał Księżuk <michal@ksiezuk.pl>
2022-06-19 16:18:42 +0200

    Extended Tailwind config with custom theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "super-blue-light": "#1652f0",
+        "super-blue-dark": "#27336e",
+        "super-blue-darker": "#080d3a",
+        "super-pink-light": "#ef5dac",
+        "super-pink-dark": "#ee0a81",
+        "super-grey-dark": "#636c94",
+      },
+    },
   },
   plugins: [
     require("tw-elements/dist/plugin"),
